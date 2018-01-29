657ce55a79107063dded5a5d144f056f01a6dfc1
Anthony <anthony@MacBook-Pro-de-Anthony.local>
2018-01-29 01:41:37 +0100

    zoom corrected

--- srcs/event_mouse.c
+++ srcs/event_mouse.c
@@ -6,7 +6,7 @@
 /*   By: aledru <aledru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 14:13:38 by aledru            #+#    #+#             */
-/*   Updated: 2018/01/28 21:12:03 by aledru           ###   ########.fr       */
+/*   Updated: 2018/01/29 01:39:46 by aledru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,13 @@ int		mlx_zoom(int button, int x, int y, t_fractol *fract)
 {
 	t_point_double	*tmp;
 
-	tmp = create_point_d(0.0, 0.0);
 	if (button == MOUSE_UP || (button == MOUSE_DOWN && fract->zoom >= 250))
 	{
-		set_point_d(tmp, x / fract->zoom + fract->min->x,
+		tmp = create_point_d(x / fract->zoom + fract->min->x,
 							y / fract->zoom + fract->min->y);
 		if (button == MOUSE_DOWN)
 		{
-			fract->zoom *= 0.9;
+			fract->zoom /= 1.1;
 			mlx_key_pressed(KEY_MINUS, fract);
 		}
 		if (button == MOUSE_UP)
@@ -31,21 +30,18 @@ int		mlx_zoom(int button, int x, int y, t_fractol *fract)
 			fract->zoom *= 1.1;
 			mlx_key_pressed(KEY_PLUS, fract);
 		}
-		/*fract->min->x = -2.1 - (-0.000021 * x * fract->zoom);
-		fract->min->y = -1.2 - (-0.000028 * y * fract->zoom);*/
-		fract->min->x = tmp->x - (WIN_WIDTH / fract->zoom) / 2;
-		fract->min->y = tmp->y - (WIN_HEIGHT / fract->zoom) / 2;
+		fract->min->x += tmp->x - (x / fract->zoom + fract->min->x);
+		fract->min->y += tmp->y - (y / fract->zoom + fract->min->y);
 		draw_fractal(fract);
+		ft_memdel((void*)&tmp);
 	}
-	ft_memdel((void*)&tmp);
 	return (0);
 }
 
 int		mlx_move(int x, int y, t_fractol *fract)
 {
 	int	i;
-	printf("%d\n", x);
-	printf("%d\n", y);
+
 	if (ft_strcmp(fract->name, "julia") == 0 && fract->mouse_disabled == 0)
 	{
 		if (x < 0 || x > WIN_WIDTH || y < 0 || y > WIN_HEIGHT)
